36f8bb6e3930c389fdcccabe6a9ea64a2ca6cf85
Corentin <vasseur.corentin@gmail.com>
2020-11-07 14:04:45 +0000

    FIX add infra session-score to interact with PostgreSQL throught ORM (example in domain/appartement.py)

--- src/colibrimmo/pipelines/data_acquisition/pipeline.py
+++ src/colibrimmo/pipelines/data_acquisition/pipeline.py
@@ -1,20 +1,45 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import create_fake_appartement, update_ads
+from .nodes import update_tables
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(
-                update_ads,
-                ["ads", "params:env"],
+                update_tables,
+                "ads",
                 "sql-ads"
             ),
-            # node(
-            #     create_fake_appartement,
-            #     ["params:iris_code", "params:parution_date", "params:description"],
-            #     "status",
-            # )
+            node(
+                update_tables,
+                "zones-cities",
+                "sql-zones-cities"
+            ),
+            node(
+                update_tables,
+                "zones-departments",
+                "sql-zones-departments"
+            ),
+            node(
+                update_tables,
+                "zones-insee",
+                "sql-zones-insee"
+            ),
+            node(
+                update_tables,
+                "zones-postal-codes",
+                "sql-zones-postal-codes"
+            ),
+            node(
+                update_tables,
+                "zones-regions",
+                "sql-regions"
+            ),
+            node(
+                update_tables,
+                "zones-iris",
+                "sql-iris"
+            ),
         ]
     )
